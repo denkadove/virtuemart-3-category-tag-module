425070a108bf6a6bbfe0875288eac391a8740a1a
Denis Sunflower <denkadove@gmail.com>
2023-03-14 10:33:42 +0300

    virtuemart 4 fix

--- mod_vm_cat_as_tag_slider/mod_vm_cat_as_tag_slider.php
+++ mod_vm_cat_as_tag_slider/mod_vm_cat_as_tag_slider.php
@@ -1,8 +1,7 @@
 <?php
 defined('_JEXEC') or  die( 'Direct Access to '.basename(__FILE__).' is not allowed.' );
-
-
 defined('DS') or define('DS', DIRECTORY_SEPARATOR);
+
 if (!class_exists( 'VmConfig' )) require(JPATH_ROOT.DS.'administrator'.DS.'components'.DS.'com_virtuemart'.DS.'helpers'.DS.'config.php');
 
 VmConfig::loadConfig();
@@ -27,65 +26,56 @@ function unique_multidim_array($array, $key) {
         $i++; 
     } 
     return $temp_array; 
-} 
-
-
+}
 
 $actual_category_number = JRequest::getVar('virtuemart_category_id', null);
-
 
 function get_icon_list($actual_category_number){
     $cat = VmModel::getModel('category');
     $productModel = VmModel::getModel('Product');
 
     $prod_list = $productModel->getProductsListing($group = FALSE, $nbrReturnProducts = 500,
-                                                $withCalc = TRUE, $onlyPublished = TRUE, 
-                                                $single = FALSE, $filterCategory = TRUE, 
-                                                $category_id = $actual_category_number, $filterManufacturer = TRUE, 
+                                                $withCalc = TRUE, $onlyPublished = TRUE,
+                                                $single = FALSE, $filterCategory = TRUE,
+                                                $category_id = $actual_category_number, $filterManufacturer = TRUE,
                                                 $manufacturer_id = 0, $omit = 0);
     $category_list = array();
 
         //получаем список id продуктов на странице
         foreach ($prod_list as $i => $value) {
-            $product_id_list[$i] = $value->virtuemart_product_id;            
+            $product_id_list[$i] = $value->virtuemart_product_id;
         }
 
         //для каждого продукта получаем список категорий
         foreach ($product_id_list as $i => $value) {
             $cat_list[$i] = $productModel->getProductCategories($product_id_list[$i]);
 
-       //для каждой категории получаем id, имя и metakey (используется в качестве названия бейджа)
+            //для каждой категории получаем id, имя и metakey (используется в качестве названия бейджа)//
             foreach ($cat_list[$i] as $j => $value) {
-                $id = $cat_list[$i][$j][virtuemart_category_id];                
-                $name = $cat_list[$i][$j][category_name];
-                $alt = $cat_list[$i][$j][metakey];
-                $published = $cat_list[$i][$j][published];
+                $id = $cat_list[$i][$j]['virtuemart_category_id'];
+                $name = $cat_list[$i][$j]['category_name'];
+                $alt = $cat_list[$i][$j]['metakey'];
+                $published = $cat_list[$i][$j]['published'];
 
-                //vardump($published);
-
-                if (!$alt){
-                    $array = array('id' => $id, 'name' => $name, 'alt'=> $name);
+                if (!$alt) {
+                    $array = array('id' => $id, 'name' => $name, 'alt' => $name);
                 } else {
-                    $array = array('id' => $id, 'name' => $alt, 'alt'=> $name);
-                } 
+                    $array = array('id' => $id, 'name' => $alt, 'alt' => $name);
+                }
 
-                if ($published){
-                    array_push($category_list,$array);
-                }                
-                        
+                if ($published) {
+                    array_push($category_list, $array);
+                }
             }
         }
-        
-        $unique_category_list = unique_multidim_array($category_list,'id');
-        //$unique_category_list = shuffle($category_list);
-        return $unique_category_list;
+        return unique_multidim_array($category_list,'id');
 }
 
 $url = ((!empty($_SERVER['HTTPS'])) ? 'https' : 'http') . '://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
 
 $cache = JFactory::getCache('cat_tag_mod', '');
 $cache->setCaching(true); // Force cache enable
-$cache->setLifeTime(600); // Minutes
+$cache->setLifeTime(1500); // Minutes
 $cacheKey = serialize($url);
  
 if (!($unique_category_list = $cache->get($cacheKey, '')))
@@ -98,4 +88,3 @@ $doc = JFactory::getDocument();
 $doc->addStyleSheet(JURI::base().'modules/mod_vm_cat_as_tag_slider/assets/vm_cat_as_tag_slider.css');
 $doc->addStyleDeclaration($style);
 require(JModuleHelper::getLayoutPath('mod_vm_cat_as_tag_slider',$layout));
-?>
--- mod_vm_cat_as_tag_slider/tmpl/default.php
+++ mod_vm_cat_as_tag_slider/tmpl/default.php
@@ -1,9 +1,8 @@
 <?php // no direct access
 defined('_JEXEC') or die('Restricted access');
 $document = JFactory::getDocument();
-$meta = '';
-$document->setMetadata('keywords', $meta );
-$view = JRequest::getVar('view', null); 
+$document->setMetadata('keywords', '' );
+$view = JRequest::getVar('view', null);
 
 if (($view !== "productdetails") && (count($unique_category_list)>'1')){
 ?>
@@ -11,26 +10,20 @@ if (($view !== "productdetails") && (count($unique_category_list)>'1')){
     <input type="checkbox" class="caterory_tag_read_more_checker" id="caterory_tag_read_more_checker" />
     <div class="caterory_tag_limiter">
         <div class="tag_container" id="tag_container">
-            <?php foreach ($unique_category_list as $i => $value) {
-                $caturl = JRoute::_('index.php?option=com_virtuemart&view=category&virtuemart_category_id='.$value['id']);
-                $cattext = $value['name'];
-                $alt = array('alt' => $value['alt']);
-            
-            if(!in_array($category->virtuemart_category_id, $id)){ ?>            
-                <div class="vmcat_tag-block">
-                    <?php if($img){ ?>
-                        <div class="vmcat_tag-image"></div>
-                    <?php } ?>
-                    <div class="vmcat_tag-name">
-                        <?php echo JHTML::link($caturl, $cattext, $alt); ?>
-                        <?php if($count) {
-                            $countProduct = $categoryModel->countProducts($category->virtuemart_category_id);
-                            echo '<span>('.$countProduct.')</span>';
-                        } ?>                           
+            <?php
+                $unique_category_list = get_icon_list($actual_category_number);
+                foreach ($unique_category_list as $i => $value) {
+                    $caturl = JRoute::_('index.php?option=com_virtuemart&view=category&virtuemart_category_id='.$value['id']);
+                    $cattext = $value['name'];
+                    $alt = array('alt' => $value['alt']);
+             ?>
+                    <div class="vmcat_tag-block">
+                        <div class="vmcat_tag-name">
+                            <?php echo JHTML::link($caturl, $cattext, $alt); ?>
+                        </div>
                     </div>
-                </div>
             <?php } ?>
-            <?php } ?>  
+
         </div>
     <!-- Здесь сам контент который должен будет скрываться / раскрываться -->
         <div class="caterory_tag_bottom"></div>
